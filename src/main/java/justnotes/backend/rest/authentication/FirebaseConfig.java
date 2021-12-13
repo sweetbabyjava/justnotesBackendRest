ad012ee9404972e3dcd71ff05ad78d95c4986d2a
Marc Trankalis <Marc.Trankalis@haw-hamburg.de>
2021-12-13 11:28:02 +0100

    "weekend flow"

--- src/main/java/justnotes/backend/rest/authentication/FirebaseConfig.java
+++ src/main/java/justnotes/backend/rest/authentication/FirebaseConfig.java
@@ -9,8 +9,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.core.io.ClassPathResource;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 
 @Configuration
 public class FirebaseConfig {
@@ -21,11 +24,20 @@ public class FirebaseConfig {
     public void firebaseInit() {
         InputStream inputStream = null;
         try {
-            inputStream = new ClassPathResource("firebase_config.json").getInputStream();
+            if(System.getenv("justNotes_Firebase_Config") != null
+                    && !System.getenv("justNotes_Firebase_Config").isBlank()){
+                inputStream = new ByteArrayInputStream(System.getenv("justNotes_Firebase_Config")
+                                .getBytes(StandardCharsets.UTF_8));
+            }
+            else{
+                inputStream = new ClassPathResource("firebase_config.json").getInputStream();
+            }
+
         } catch (IOException e3) {
             e3.printStackTrace();
         }
         try {
+            assert inputStream != null;
             FirebaseOptions options = FirebaseOptions.builder()
                             .setCredentials(GoogleCredentials
                             .fromStream(inputStream))
